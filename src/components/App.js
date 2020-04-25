0bee3791a06b00d6309d51a74420466ecfef18d6
osgood gunawan <ogunawan@ucsd.edu>
2020-04-25 13:37:37 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,76 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import Filter from "./Filter"
+// import HelloWorld from "./HelloWorld";
+import HogContainer from "./HogContainer";
 
 class App extends Component {
+
+  state={
+    hogs : hogs,
+    greased : false,
+    sort : "unsorted"
+  }
+
+  toggleGreased=()=>{
+    this.setState({
+      greased: !this.state.greased
+    })
+  }
+
+  handleSort= event =>{
+    this.setState({
+      sort: event.target.value
+    })
+  }
+
+
   render() {
+    const {greased,sort}=this.state
+
+    // made a copy of the sate, making sure we're not mutating the original
+    let displayHogs=[...this.state.hogs]
+
+    // {greased ? displayHogs=displayHogs.filter(hog=>hog.greased) : null}
+
+    // this is not working, just want to make unsorted as default so we can take out the unsorted in the drop down 
+    // switch (sort){
+
+    //   case "weight":
+    //     return  displayHogs.sort((a,b) => a.weight - b.weight);
+
+    //   case "name":
+    //     return displayHogs.sort((a,b)=>a.name.toLowerCase() > b.name.toLowerCase()? 1 : -1);
+
+    //   default:
+    //      displayHogs=displayHogs.filter(hog => hog.greased);
+
+
+    // }
+
+// greased or not greased filter button
+    if(greased){
+      displayHogs=displayHogs.filter(hog => hog.greased)
+    }
+
+   if(sort==='weight'){
+     displayHogs.sort((a,b) => a.weight - b.weight)
+    }else if (sort==='name'){
+      displayHogs.sort((a,b)=>a.name.toLowerCase() > b.name.toLowerCase()? 1 : -1)
+    }
+
+
     return (
       <div className="App">
-        <Nav />
-        <HelloWorld />
+      <Nav />
+      <Filter
+       toggleGreased={this.toggleGreased}
+       greased={greased}
+       handleSort={this.handleSort}
+       sort={sort}
+      />
+      <HogContainer hog={displayHogs} />
       </div>
     );
   }
---
+++ src/components/HogContainer.js
@@ -0,0 +1,44 @@
+import React from  'react'
+import PropTypes from 'prop-types'
+import HogDetail from './HogDetail'
+
+
+export default class HogContainer extends React.Component {
+    
+
+    
+    renderHogs = () => {
+        const {hog}=this.props
+        return (hog.map(hog => {
+           return <HogDetail key={hog.id}{...hog} />
+        })
+        )
+    }
+
+    // {...hog} is the equilvalent to code below,
+    // is just copy of the hog object with the same key /value  (spread operator)
+    
+    // name={hog.name}
+    // specialty={hog.specialty}
+    // greased={hog.greased}
+    // weight={hog.weight}
+    // highestMedalAchieved={hog['highest medal achieved']}
+
+
+    render(){
+        
+        return (
+
+        <div className="ui grid container">
+             {this.renderHogs()}
+        </div> 
+
+        )
+        
+    }
+}
+
+HogContainer.PropTypes={
+    hog: PropTypes.object.isRequired
+}
+
